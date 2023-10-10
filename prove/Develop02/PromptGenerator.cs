63bf3436ea3b899f088c7bcdff828a6baf2e879b
23samantabaez <23sbaezgomez@gmail.com>
2023-10-09 22:32:53 -0400

    del

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,39 +1,3 @@
-/*public class PromptGenerator
-{
-    public string RandomPrompt()
-    {
-        //choose from list of random prompts and display to user
-        Random randomPrompt = new Random();
-        int promptNum = randomPrompt.Next(1, 5);
-        int prompt = Convert. ToInt32(Console.ReadLine()); //to convert, maybe?
-
-        if (promptNum == 1)
-        {
-        Console.WriteLine("Who was the most interesting person I interacted with today?");
-        }
-
-        if (promptNum == 2)
-        {
-            Console.WriteLine("What was the best part of my day?");
-        }
-
-        if (promptNum == 3)
-        {
-            Console.WriteLine("How did I see the hand of the Lord in my lelse ife today?");
-        }
-
-        if (promptNum == 4)
-        {
-            Console.WriteLine("What was the strongest emotion I felt today?");
-        }
-
-        if (promptNum == 5)
-        {
-            Console.WriteLine("if I had one thing I could do over today, what would it be?");
-        }   
-    }   
-}*/
-
 public class Prompt
 {
     public List<string> _promptList = new List<string>() {
